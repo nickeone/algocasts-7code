3486ebc8973674b5097bec75f628cbdbe74f4448
nickeone <nicu.mardari@yahoo.com>
2019-06-22 13:28:36 +0300

    'reversestring-2-3'

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,12 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+// SOLUTION 1
+
+// function reverse(str) {
+//     var reversed = '';
+//
+//     for (let char of str) {
+//         reversed =  char + reversed;
+//         console.log(reversed);
+//
+//     }
+//     return reversed;
+// }
+// SOLUTION 2
+
+// function reverse(str) {
+//     var reversed = '';
+//     for(var i = str.length-1; i >= 0; i--){
+//         reversed += str[i]
+//     }
+//     return reversed;
+// }
+
+// SOLUTION 3
+
 function reverse(str) {
-    var reversed = '';
-    for(var i = str.length-1; i >= 0; i--){
-        reversed += str[i]
-    }
-    return reversed;
+    return str.split('').reduce( (accumulator, currentValue) => {
+        console.log(accumulator);
+        return currentValue + accumulator}, '');
 }
+
+
+
 reverse('nicu');
 module.exports = reverse;
