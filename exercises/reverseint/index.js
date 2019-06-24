9cc433ff995559388ff7bf83f3ad711507906940
nickeone <nicu.mardari@yahoo.com>
2019-06-24 19:34:33 +0300

    reverseint-2

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,17 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+// SOLUTION 1
+// function reverseInt(n) {
+//     let reversed = '';
+//     let str = n.toString();
+//     for( let nr of str){
+//         reversed = nr + reversed;
+//     }
+//     console.log(Number(reversed));
+//     if(n < 0) {
+//         return parseInt(reversed) * -1;
+//     }
+//     return parseInt(reversed);
+// }
+
+// SOLUTION 2
 function reverseInt(n) {
-    let reversed = '';
-    let str = n.toString();
-    for( let nr of str){
-        reversed = nr + reversed;
-    }
-    console.log(Number(reversed));
-    if(n < 0) {
-        return parseInt(reversed) * -1;
-    }
-    return parseInt(reversed);
+    let reversed = n.toString().split('').reverse().join('');
+    console.log(reversed);
+    return parseInt(reversed) * Math.sign(n);
 }
 
 reverseInt(1224);
