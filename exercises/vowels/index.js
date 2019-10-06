42cd142078320cb6b4154dc5225a782bd69405c2
nickeone <nicu.mardari@yahoo.com>
2019-10-06 21:51:58 +0300

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let lower = str.toLowerCase();
+    console.log(lower);
+    let strVowels = '';
+    let vowels = "aeiou";
+    for(let i = 0; i < lower.length; i++){
+        for(let j = 0; j < vowels.length; j++){
+            if(lower[i] === vowels[j]){
+                strVowels += (str[i]);
+            }
+        }
+    }
+    console.log('nr: ', strVowels.length);
+    return strVowels.length;
+}
 
+vowels("IE Nicu is here");
 module.exports = vowels;
